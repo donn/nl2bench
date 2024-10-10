9b237f6b15fa33fbf1a6e7c08ef12a9102af7cc5
Mohamed Gaber <donn@efabless.com>
2024-10-10 11:08:42 +0300

    Compress all test cases

--- test/test_nl2bench.py
+++ test/test_nl2bench.py
@@ -3,6 +3,7 @@ import pytest
 import subprocess
 from tempfile import NamedTemporaryFile
 from nl2bench.nl2bench import verilog_netlist_to_bench
+import gzip
 
 
 @pytest.mark.parametrize(
@@ -17,12 +18,14 @@ def testy_basic(
     scl,
     design,
 ):
-    netlist = os.path.join(pytest.test_root, "designs", design, scl, "nl.v")
-    if not os.path.exists(netlist):
-        netlist = os.path.join(pytest.test_root, "designs", design, scl, "nl.v.gz")
+    netlist = os.path.join(pytest.test_root, "designs", design, scl, "nl.v.gz")
     lib = os.path.join(pytest.test_root, "tech", f"{scl}.lib")
-    expected = os.path.join(pytest.test_root, "designs", design, scl, "nl.bench")
-    with NamedTemporaryFile("w", suffix=".bench") as f:
+    expected = os.path.join(pytest.test_root, "designs", design, scl, "nl.bench.gz")
+    with NamedTemporaryFile("w", suffix=".bench") as f, NamedTemporaryFile(
+        "wb", suffix=".expected.bench"
+    ) as expected_x, gzip.open(expected, "rb") as expected_c:
+        expected_x.write(expected_c.read())
+
         verilog_netlist_to_bench(netlist, [lib], f)
         f.flush()
-        subprocess.check_call(["quaigh", "equiv", expected, f.name])
+        subprocess.check_call(["quaigh", "equiv", expected_x.name, f.name])
